2defcb570e4e5bf87b5b1cc909393ca218100ce1
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-12-16 21:25:38 +0300

    Добавил компонент подробного описания продукта

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import Cart from '../cart/cart';
 import MainPage from '../main-page/main-page';
+import PropertyProductCard from '../property-product-card/property-product-card';
 
 function App(): JSX.Element {
   return (
@@ -11,6 +12,9 @@ function App(): JSX.Element {
       </Route>
       <Route path={AppRoute.Cart} exact>
         <Cart />
+      </Route>
+      <Route path={AppRoute.Product} exact>
+        <PropertyProductCard />
       </Route>
     </Switch>
   );
--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import Footer from '../footer/footer';
