9dc8cebcb5b056c28ba019122fd32e6a1d27b216
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-12-17 17:36:59 +0300

    Подкллючил страницу каталога к стейту

--- src/components/product-cards-list/product-cards-llist.tsx
+++ src/components/product-cards-list/product-cards-llist.tsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
+import { getGuitars } from '../../store/selectors';
 import ProductCardItem from '../prodact-card-item/prodact-card-item';
 
 function ProductCardsList(): JSX.Element {
+  const guitars = useSelector(getGuitars);
+
   return (
     <div className="cards catalog__cards">
-      <ProductCardItem />
+      {guitars.map((guitar) => <ProductCardItem key={guitar.id} />)}
     </div>
   );
 }
