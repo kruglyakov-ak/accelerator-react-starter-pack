4a484254b3c13297e53b68ceb82a8ddce3482fc4
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-12-17 15:33:48 +0300

    Реализовал axios api

---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+
+const BACKEND_URL = 'https://accelerator-guitar-shop-api-v1.glitch.me';
+const REQUEST_TIMEOUT = 5000;
+
+const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error: AxiosError) => Promise.reject(error),
+  );
+
+  return api;
+};
+
+export { createAPI };
