db6f2a205e0f45bc60e02a445d612ff3aec57302
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-12-31 02:16:47 +0300

    Написал тесты для компонента ProductCardItem

---
+++ src/components/prodact-card-item/prodact-card-item.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createAPI } from '../../services/api';
+import { State } from '../../types/state';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import ProductCardItem from './prodact-card-item';
+import { GuitarType } from '../../const';
+
+const history = createMemoryHistory();
+const api = createAPI();
+const middlewares = [thunk.withExtraArgument(api)];
+const mockStore = configureMockStore<
+  State,
+  Action,
+  ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  DATA: {
+  },
+});
+
+describe('Component: ProductCardItem', () => {
+
+  it('should render ProductCardItem', () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <ProductCardItem guitar={{
+            id: 0,
+            name: 'Гитара',
+            vendorCode: '',
+            type: GuitarType.Electric,
+            description: '',
+            previewImg: '',
+            stringCount: 0,
+            rating: 0,
+            price: 1000,
+          }}
+          />
+        </Router>
+      </Provider>);
+
+    expect(screen.getByText(/Гитара/i)).toBeInTheDocument();
+    expect(screen.getByText(/1000/i)).toBeInTheDocument();
+  });
+
+});
