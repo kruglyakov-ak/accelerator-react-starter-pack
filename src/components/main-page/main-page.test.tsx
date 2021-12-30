bcb5a1f8a116f4c5ed042156d3c52c2673bb8c80
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-12-31 01:15:18 +0300

    Написал тесты для компонента MainPage

---
+++ src/components/main-page/main-page.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createAPI } from '../../services/api';
+import { State } from '../../types/state';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import MainPage from './main-page';
+
+const history = createMemoryHistory();
+const api = createAPI();
+const middlewares = [thunk.withExtraArgument(api)];
+const mockStore = configureMockStore<
+  State,
+  Action,
+  ThunkDispatch<State, typeof api, Action>
+>(middlewares);
+
+const store = mockStore({
+  FILTER: {
+  },
+  DATA: {
+    guitars: [],
+  },
+  SORT: {
+  },
+  PAGINATION:{
+    guitarsCount: 0,
+  },
+});
+
+describe('Component: MainPage', () => {
+
+  it('should render MainPage', () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <MainPage />
+        </Router>
+      </Provider>);
+
+    expect(screen.getByText(/Каталог гитар/i)).toBeInTheDocument();
+  });
+
+});
