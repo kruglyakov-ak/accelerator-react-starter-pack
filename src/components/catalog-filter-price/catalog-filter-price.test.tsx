45b34ba3f0a68b7ae2fbe3125b94aad88e95abb9
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-12-31 00:38:02 +0300

    Написал тесты для компонента CatalogFilterPrice

--- src/components/catalog-filter-price/catalog-filter-price.test.tsx
+++ src/components/catalog-filter-price/catalog-filter-price.test.tsx
@@ -1,16 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createAPI } from '../../services/api';
 import { State } from '../../types/state';
 import thunk, { ThunkDispatch } from 'redux-thunk';
 import { Action } from 'redux';
 import { configureMockStore } from '@jedmao/redux-mock-store';
-import { createMemoryHistory } from 'history';
-import CatalogFilter from './catalog-filter';
 import { DefaultPriceRange } from '../../const';
+import CatalogFilterPrice from './catalog-filter-price';
 
-const history = createMemoryHistory();
 const api = createAPI();
 const middlewares = [thunk.withExtraArgument(api)];
 const mockStore = configureMockStore<
@@ -25,19 +22,16 @@ const store = mockStore({
     priceRangeMax: DefaultPriceRange.Max,
   },
 });
-describe('Component: CatalogFilter', () => {
+describe('Component: Cart', () => {
 
-  it('should render correctly', () => {
+  it('should render CatalogFilterPrice', () => {
     render(
       <Provider store={store}>
-        <Router history={history}>
-          <CatalogFilter />
-        </Router>
+        <CatalogFilterPrice />
       </Provider>);
 
-    expect(screen.getByText(/Фильтр/i)).toBeInTheDocument();
-    expect(screen.getByText(/Тип гитар/i)).toBeInTheDocument();
-    expect(screen.getByText(/Количество струн/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(DefaultPriceRange.Min)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(DefaultPriceRange.Max)).toBeInTheDocument();
   });
 
 });
