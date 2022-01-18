47598af0da8624426f33b33ad7cc010aea52dce7
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-01-18 20:07:36 +0300

    Исправил тесты для редьюсера pagePagination

--- src/store/page-pagination/page-pagination.test.ts
+++ src/store/page-pagination/page-pagination.test.ts
@@ -1,24 +1,21 @@
 import { setGuitarsCount } from '../action';
 import { pagePagination } from './page-pagination';
 
-describe('Reducer: page-pagination', () => {
+describe('Reducer: pagePagination', () => {
   it('without additional parameters should return initial state', () => {
     expect(pagePagination(void 0, { type: 'UNKNOWN_ACTION' }))
       .toEqual({
         guitarsCount: 0,
-        currentPageNumber: 0,
       });
   });
 
   it('should update guitarsCount by load guitars', () => {
     const state = {
       guitarsCount: 0,
-      currentPageNumber: 0,
     };
     expect(pagePagination(state, setGuitarsCount(1)))
       .toEqual({
         guitarsCount: 1,
-        currentPageNumber: 0,
       });
   });
 });
